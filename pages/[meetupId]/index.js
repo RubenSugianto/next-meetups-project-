48d7527df7e3048e0cd11c211f104baca8c398a5
RubenSugianto <ruben.sugianto@gmail.com>
2025-05-04 12:27:56 +0700

    Adding Custom Components & CSS Modules

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,1 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
+function MeetupDetails() {
+    return (
+        <MeetupDetail 
+        title= 'Cristiano Ronaldo Fan Meetup'
+        image= 'https://akcdn.detik.net.id/community/media/visual/2024/11/06/cristiano-ronaldo-1.jpeg?w=600&q=90'
+        address= 'Juventus Stadium, Turin, Italy'
+        description= 'Join fellow fans of Cristiano Ronaldo for a fun evening celebrating his legendary football career with games, trivia, and memorabilia.'
+        />
+    );
+}
+
+export default MeetupDetails;
